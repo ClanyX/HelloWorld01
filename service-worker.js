5bf14bc005a5a2b13d77ff30848c23e5839e5628
Šandera Filip <L20220082@SSPTAJI.CZ>
2024-09-30 10:18:25 +0200

    service-worker edit

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,41 @@
 // Registrace service workeru
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-    console.log('Service Worker registrov´an s n´asleduj´ıc´ım scope:', registration.scope);
-    }, err => {
-    console.log('Service Worker registrace selhala:', err);
+      navigator.serviceWorker.register('/sw.js').then(registration => {
+        console.log('Service Worker registrován s následujícím scope:', registration.scope);
+      }, err => {
+        console.log('Service Worker registrace selhala:', err);
+      });
     });
-    });
-    }
-    // Instalace service workeru
-    self.addEventListener('install', event => {
+  }
+  
+  // Instalace service workeru
+  self.addEventListener('install', event => {
     console.log('Service Worker se instaluje...');
     event.waitUntil(
-    caches.open('my-cache').then(cache => {
-    return cache.addAll([
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js'
-    ]);
-    })
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          '/icons/icon-192x192.png',
+          '/icons/icon-512x512.png'
+        ]);
+      })
     );
-    });
-    // Aktivace service workeru
-    self.addEventListener('activate', event => {
-    console.log('Service Worker byl aktivov´an');
-    });
-    // Zachyt´av´an´ı poˇzadavk˚u
-    self.addEventListener('fetch', event => {
+  });
+  
+  // Aktivace service workeru
+  self.addEventListener('activate', event => {
+    console.log('Service Worker byl aktivován');
+  });
+  
+  // Zachytávání požadavků
+  self.addEventListener('fetch', event => {
     event.respondWith(
-    caches.match(event.request).then(response => {
-    return response || fetch(event.request);
-    })
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
     );
-    }); 
+  });
