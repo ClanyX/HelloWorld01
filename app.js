f52783d1a5ae03b520b811a6336bea83fb5e467d
Šandera Filip <L20220082@SSPTAJI.CZ>
2024-09-30 11:24:31 +0200

    edit 2

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ if ('serviceWorker' in navigator) {
     });
 }
  
-function sayHello() {
-    document.getElementById('hello').innerText = 'Hello world';
+function count(){
+    const operant1 = parseFloat(document.getElementById('operant1').value);
+    const operant2 = parseFloat(document.getElementById('operant2').value);
+    const operation = document.getElementById('operation').value;
+    var result;
+
+    switch(operation){
+        case '+':
+            result = operant1 + operant2;
+            break;
+        case '-':
+            result = operant1 - operant2;
+            break;
+        case "*":
+            result = operant1 * operant2;
+            break;
+        case "/":
+            result = operant1 / operant2;
+            break;
+        default:
+            console.log("spatna operace");
+            break;
+    }
 }
